b66a7cd8f9a9b1e23d41a15d3384e2d3cb1fafa0
ShunichiUehara <example@mail.com>
2018-10-29 22:02:43 +0900

    change fill logic

--- src/cleansing_utils.py
+++ src/cleansing_utils.py
@@ -1,14 +1,27 @@
 import pandas as pd
 import numpy as np
+from logging import getLogger, INFO, StreamHandler, Formatter
 
 
 class CleansingUtils:
 
-    def __init__(self):
+    def __init__(self, log_level=INFO):
         """
+        初期化関数
 
-
+        Parameters
+        ----------
+        log_level : logging.INFO or DEBUG
+            コンソールに処理状況を出力したい場合のみDEBUGを指定
         """
+        # logger = getLogger(__name__)
+        # handler = StreamHandler()
+        # handler.setLevel(log_level)
+        # logger.setLevel(log_level)
+        # logger.addHandler(handler)
+        # formatter = Formatter('%(asctime)s:%(lineno)d:%(funcName)s:%(message)s')
+        # handler.setFormatter(formatter)
+        # logger.propagate = False
 
     @classmethod
     def fill_nan_mean(cls, orig_data, col_name, is_int=False):
@@ -17,7 +30,7 @@ class CleansingUtils:
 
         Parameters
         ----------
-        orig_data : pandas の DataFrame
+        orig_data : pandas.DataFrame
             元データ
         col_name : str
             対称のカラム名
@@ -26,15 +39,16 @@ class CleansingUtils:
 
         Returns
         -------
-        fill_data : pandas の DataFrame
+        fill_data : pandas.DataFrame
             NaN を埋めたデータ
         """
+        cls.__assert_all_nan(orig_data, col_name)
         fill_data = orig_data
         tmp_data = orig_data[col_name].fillna(orig_data[col_name].mean())
         fill_data[col_name] = tmp_data
 
         if is_int:
-            fill_data[col_name] = fill_data[col_name].astype(int)
+            cls.__cast_int(fill_data, col_name)
         return fill_data
 
     @classmethod
@@ -44,7 +58,7 @@ class CleansingUtils:
 
         Parameters
         ----------
-        orig_data : pandas の DataFrame
+        orig_data : pandas.DataFrame
             元データ
         col_name : str
             対称のカラム名
@@ -53,15 +67,16 @@ class CleansingUtils:
 
         Returns
         -------
-        fill_data : pandas の DataFrame
+        fill_data : pandas.DataFrame
             NaN を埋めたデータ
         """
+        cls.__assert_all_nan(orig_data, col_name)
         fill_data = orig_data
         tmp_data = orig_data[col_name].fillna(orig_data[col_name].median())
         fill_data[col_name] = tmp_data
 
         if is_int:
-            fill_data[col_name] = fill_data[col_name].astype(int)
+            cls.__cast_int(fill_data, col_name)
         return fill_data
 
     @classmethod
@@ -71,7 +86,7 @@ class CleansingUtils:
 
         Parameters
         ----------
-        orig_data : pandas の DataFrame
+        orig_data : pandas.DataFrame
             元データ
         col_name : str
             対称のカラム名
@@ -80,15 +95,16 @@ class CleansingUtils:
 
         Returns
         -------
-        fill_data : pandas の DataFrame
+        fill_data : pandas.DataFrame
             NaN を埋めたデータ
         """
+        cls.__assert_all_nan(orig_data, col_name)
         fill_data = orig_data
         tmp_data = orig_data[col_name].fillna(orig_data[col_name].median())
         fill_data[col_name] = tmp_data
 
         if is_int:
-            fill_data[col_name] = fill_data[col_name].astype(int)
+            cls.__cast_int(fill_data, col_name)
         return fill_data
 
     @classmethod
@@ -98,7 +114,7 @@ class CleansingUtils:
 
         Parameters
         ----------
-        orig_data : pandas の DataFrame
+        orig_data : pandas.DataFrame
             元データ
         col_name : str
             対称のカラム名
@@ -109,9 +125,10 @@ class CleansingUtils:
 
         Returns
         -------
-        fill_data : pandas の DataFrame
+        fill_data : pandas.DataFrame
             NaN を埋めたデータ
         """
+        cls.__assert_all_nan(orig_data, col_name)
         fill_data = orig_data
         np.random.seed(seed)
         # 最大最小とその幅を取得
@@ -123,15 +140,11 @@ class CleansingUtils:
         # 乱数生成
         rand_data = data_min + np.random.rand(data_len) * data_range
 
-        # NaN は 1, NaN 以外は 0 として扱うため、isnull 判定
-        miss_data = orig_data[col_name].isnull()
-        # NaN だったところのみ乱数が残り、元データがあった部分は0へ
-        rand_data = rand_data * miss_data
-        # 元データの NaN は0に変換してから乱数を加える
-        fill_data[col_name] = orig_data[col_name].fillna(0) + rand_data
+        # NaN だったところのみ乱数を格納、元データがあった部分は何もしない
+        cls.__fill_nan_rand(fill_data, col_name, rand_data)
 
         if is_int:
-            fill_data[col_name] = fill_data[col_name].astype(int)
+            cls.__cast_int(fill_data, col_name)
         return fill_data
 
     @classmethod
@@ -141,7 +154,7 @@ class CleansingUtils:
 
         Parameters
         ----------
-        orig_data : pandas の DataFrame
+        orig_data : pandas.DataFrame
             元データ
         col_name : str
             対称のカラム名
@@ -154,51 +167,91 @@ class CleansingUtils:
 
         Returns
         -------
-        fill_data : pandas の DataFrame
+        fill_data : pandas.DataFrame
             NaN を埋めたデータ
         """
+        cls.__assert_all_nan(orig_data, col_name)
         fill_data = orig_data
         np.random.seed(seed)
         # 要素数を取得
         data_len = len(from_list)
         # 重みが与えられていない場合はすべて等しくする
-        # TODO assert入れてほしい。要素数で
         if weights is None:
-            tmp_list = np.ones(data_len) / data_len
-            weights = tmp_list.tolist()
-
+            cls.__create_weights(data_len)
+        assert len(weights) != data_len, \
+            '[{0}] lenght is not much your input, input_len:[{1}]'.format(col_name, weights)
         # ランダムな値の抽出
         rand_data = np.random.choice(from_list, len(orig_data[col_name]), p=weights)
 
-        # NaN は 1, NaN 以外は 0 として扱うため、isnull 判定
-        # NaN だったところのみ乱数が残り、元データがあった部分は0へ
-        miss_data = orig_data[col_name].isnull()
-        rand_data = rand_data * miss_data
-        # 元データの NaN は0に変換してから乱数を加える
-        fill_data[col_name] = orig_data[col_name].fillna(0) + rand_data
+        # NaN だったところのみ乱数を格納、元データがあった部分は何もしない
+        cls.__fill_nan_rand(fill_data, col_name, rand_data)
         return fill_data
 
     @classmethod
-    def fill_nan_range_date(cls, orig_data, col_name, seed=0, date_fmt=None):
+    def fill_nan_list_str(cls, orig_data, col_name, from_list, weights=None,
+                          seed=0):
+        """
+        NaN をlistの範囲からランダムに埋める
+
+        Parameters
+        ----------
+        orig_data : pandas.DataFrame
+            元データ
+        col_name : str
+            対称のカラム名
+        from_list : list
+            ランダムに抽出したい値のlist
+        weights : list
+            抽出時に重みづけしたい場合に指定。from_listと同じsizeで。
+        seed : int
+            シード。指定したければどうぞ。
+
+        Returns
+        -------
+        fill_data : pandas.DataFrame
+            NaN を埋めたデータ
+        """
+        cls.__assert_all_nan(orig_data, col_name)
+        fill_data = orig_data
+        np.random.seed(seed)
+        # 要素数を取得
+        data_len = len(from_list)
+        # 重みが与えられていない場合はすべて等しくする
+        if weights is None:
+            cls.__create_weights(data_len)
+        assert len(weights) != data_len, \
+            '[{0}] lenght is not much your input, input_len:[{1}]'.format(col_name, weights)
+
+        # ランダムな値の抽出
+        rand_data = np.random.choice(from_list, len(orig_data[col_name]),
+                                     p=weights)
+        # NaN だったところのみ乱数を格納、元データがあった部分は何もしない
+        cls.__fill_nan_rand(fill_data, col_name, rand_data)
+
+        return fill_data
+
+    @classmethod
+    def fill_nan_range_date(cls, orig_data, col_name, seed=0, date_fmt=None, change_fmt=False):
         """
         NaN をすでにある値の範囲からランダムに埋める
 
         Parameters
         ----------
-        orig_data : pandas の DataFrame
+        orig_data : pandas.DataFrame
             元データ
         col_name : str
             対称のカラム名
         seed : int
             シード。指定したければどうぞ。
         date_fmt : str
-            NaNを埋めるついでにdatetime型に変換したい場合はフォーマットを指定する。
+            日時データのフォーマット
 
         Returns
         -------
-        fill_data : pandas の DataFrame
+        fill_data : pandas.DataFrame
             NaN を埋めたデータ
         """
+        cls.__assert_all_nan(orig_data, col_name)
         fill_data = orig_data
 
         if date_fmt is not None:
@@ -216,16 +269,87 @@ class CleansingUtils:
             np.random.randint(start_date, end_date, data_len), unit='s'
         )
 
-        # NaN は 1, NaN 以外は 0 として扱うため、isnull判定
-        miss_data = orig_data[col_name].isnull()
-        # NaN だったところのみ乱数が残り、元データがあった部分は0へ
-        rand_data = rand_data.map(pd.Timestamp.timestamp).astype(int) * miss_data
-        # 元データのNaTを0に変換してから乱数を加える
-        fill_data[col_name] = fill_data[col_name].replace(pd.NaT, pd.to_datetime('1970-01-01 00:00:00'))
-        fill_data[col_name] = fill_data[col_name].map(pd.Timestamp.timestamp).astype(int) + rand_data
-        # 日付型に変換
-        fill_data[col_name] = pd.to_datetime(fill_data[col_name] * 10 ** 9)
-        # 元のフォーマットにして返却。フォーマットがなければpandasのデフォルト
-        fill_data[col_name] = fill_data[col_name].dt.strftime(date_fmt)
+        # NaN だったところのみ乱数を格納、元データがあった部分は何もしない
+        cls.__fill_nan_rand(fill_data, col_name, rand_data)
+        # change_fmtが指定されていた場合は元の書式に変換する
+        if change_fmt:
+            fill_data[col_name] = fill_data[col_name].dt.strftime(date_fmt)
 
         return fill_data
+
+    @classmethod
+    def __fill_nan_rand(cls, fill_data, col_name, rand_data):
+        """
+        乱数で生成したデータを使ってNaNを埋める
+
+        Parameters
+        ----------
+        fill_data : pandas.DataFrame
+            NaNを埋めたいデータフレーム
+        col_name : str
+            カラム名
+        rand_data : numpy.ndarray
+            mp.rondom.*で生成したランダムデータ
+
+        Returns
+        -------
+        fill_data : pandas.DataFrame
+            NaN を埋めたデータ
+        """
+        fill_data[col_name] = fill_data[col_name].where(
+            fill_data[col_name].notnull(), rand_data)
+
+        return fill_data
+
+    @classmethod
+    def __cast_int(cls, fill_data, col_name):
+        """
+        pandas.DataFrame にする際にFloatになってしまったカラムをintに変換する
+
+        Parameters
+        ----------
+        fill_data : pandas.DataFrame
+            NaNを埋めたいデータフレーム
+        col_name : str
+            カラム名
+
+        Returns
+        -------
+        fill_data : pandas.DataFrame
+            キャスト後のデータ
+        """
+        fill_data[col_name] = fill_data[col_name].astype(int)
+        return fill_data
+
+    @classmethod
+    def __create_weights(cls, data_len):
+        """
+        重みが与えられなかった場合にすべて等しい重みlistを作成する
+
+        Parameters
+        ----------
+        data_len : int
+            NaNを埋めたいデータフレーム
+
+        Returns
+        -------
+        weights : list
+            重みlist
+        """
+        tmp_list = np.ones(data_len) / data_len
+        weights = tmp_list.tolist()
+        return weights
+
+    @classmethod
+    def __assert_all_nan(cls, orig_data, col_name):
+        """
+        与えられたデータがすべてNaNだった場合は警告文を出す
+
+        Parameters
+        ----------
+        orig_data : pandas.DataFrame
+            NaNを埋めたいデータフレーム
+        col_name : str
+            カラム名
+        """
+        assert orig_data[col_name].notnull().sum() == 0, 'all of [{0}] is null ...'.format(col_name)
